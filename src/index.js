8a3b5d5a2ff3cd88910e1cad5c2ac758fcf8a688
unknown <loggitec4@ramblerr.ru>
2018-02-28 18:18:52 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  
+  var zero=0;
+  
+  for (var i = 5; i <= number; i=i*5) {
+   zero += Math.trunc(number/i);
+  } 
+  
+  console.log(zero);
+  return (zero);
 }
